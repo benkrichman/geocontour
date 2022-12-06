1648809991b015260eec9ca78285e5a9733b0122
Benjamin Krichman <benkrichman@gmail.com>
2022-12-05 20:18:03 -0600

    __init__ test

--- geocontour/__init__.py
+++ geocontour/__init__.py
@@ -1,9 +1,9 @@
-#from .geocontour import build
-#import geocontour.check
-#import geocontour.contourtrace
-#import geocontour.contourutil
-#import geocontour.grid
-#import geocontour.masksearch
-#import geocontour.maskutil
-#import geocontour.output
+from .geocontour import build
+import geocontour.check
+import geocontour.contourtrace
+import geocontour.contourutil
+import geocontour.grid
+import geocontour.masksearch
+import geocontour.maskutil
+import geocontour.output
 #__all__=['check','contourtrace','contourutil','geocontour','grid','masksearch','maskutil','output']
